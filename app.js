485a2222342675750922a5efa08f81b6492b5a4b
mlanca-c <marialsottomayor@gmail.com>
2023-04-07 10:25:51 +0100

    day2 - basic types

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World, my name is mlanca-c!');
+// union type is a type that can be one of several types
+function add(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combinedAges = add(30, 26);
+console.log(combinedAges);
+var combinedNames = add('mlanca-c', 'sotto');
+console.log(combinedNames);
