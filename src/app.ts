275b7bf7ce444a829e662ddf40aac09660c74bdb
mlanca-c <marialsottomayor@gmail.com>
2023-04-10 19:55:25 +0100

    day08 - decorators

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 // Decorators
 
 // decorator function
+
+// class decorator
 function Logger(logString: string) {
+  console.log("Logger");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -9,16 +12,19 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-	return function(_: Function) {
-		const hookEl= document.getElementById(hookId);
-		if (hookEl) {
-			hookEl.innerHTML = template;
-		}
-	}
+  console.log("WithTemplate");
+  return function (_: Function) {
+    console.log("Rendering template");
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  };
 }
 
-// @Logger('LOGGING - PERSON')
-@WithTemplate('<h1>My Person Object</h1>', 'app')
+// decorators execute bottom-up (their returns do)
+@Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
@@ -30,3 +36,64 @@ class Person {
 const person = new Person();
 
 console.log(person);
+
+// Property Decorators
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target);
+  console.log(propertyName);
+}
+
+// accessor decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+// method decorator
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+// parameter decorator
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  private _price: number;
+
+  constructor(private title: string, price: number) {
+    this._price = price;
+  }
+
+  @Log2
+  set price(val: number) {
+    if (val < 0) {
+      throw new Error(`${val}: invalid price`);
+    }
+    this._price = val;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+// the instantiation of the class doesn't trigger the decorators
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book 2", 29);
